d10a1f40d1647f8e82b5802f88a340c8a7b87345
Mateusz <mateuszm@mat.umk.pl>
2026-06-22 18:51:35 +0200

    SSR -> SPA

--- frontend/web/queries/users.ts
+++ frontend/web/queries/users.ts
@@ -4,7 +4,7 @@ import type {QueryClient as QueryClientType} from '@tanstack/react-query'
 import type {User} from '@shared/types/user'
 // import {apiRequest} from '@common/util/api-request'
 import {createQueryClientForServer} from '@common/util/query-client'
-import { uuidv4 } from 'zod/v4'
+import {uuidv4} from 'zod/v4'
 
 // Jeżeli server będzie już gdzieś zahostowany i będzie działał poprawnie - to trzeba będzie usunąć te mocki.
 const users: User[] = [
@@ -32,16 +32,19 @@ export const getUserQueryOptions = () => {
   return queryOptions<User[]>({
     queryKey: ['users-list'],
     // Jeżeli będzie gotowy backend to zmienić to na `queryFn: () => apiRequest('/users'),`
-    queryFn: () => { return users },
+    queryFn: () => {
+      return users
+    },
   })
 }
 
-export const fetchUserData = async () => {
-  const queryClient = createQueryClientForServer()
+// Docelowo używać będę SPA zamiast SSR, więc będzie to zakomentowane. Jakby mi się odwidziało - odkomentować
+// export const fetchUserData = async () => {
+//   const queryClient = createQueryClientForServer()
 
-  const data = await queryClient.ensureQueryData(getUserQueryOptions())
-  return {data, queryClient}
-}
+//   const data = await queryClient.ensureQueryData(getUserQueryOptions())
+//   return {data, queryClient}
+// }
 
 export const dehydrateUserState = (queryClient: QueryClientType) => {
   return dehydrate(queryClient)
--- frontend/web/routes/users.tsx
+++ frontend/web/routes/users.tsx
@@ -1,8 +1,4 @@
-import {dehydrateUserState, fetchUserData} from '@queries/users'
-
-import {HydrationBoundary} from '@tanstack/react-query'
 import type {MetaFunction} from 'react-router'
-import type {Route} from '@rr/routes/+types/users'
 import {UserPage} from '@pages/UserPage'
 import {generatePageTitle} from '@common/util/page-title'
 
@@ -10,16 +6,6 @@ export const meta: MetaFunction = () => {
   return [{title: generatePageTitle('Users')}]
 }
 
-export const loader = async () => {
-  const {queryClient} = await fetchUserData()
-  const dehydratedUserState = dehydrateUserState(queryClient)
-  return {dehydratedUserState}
-}
-
-export default ({loaderData}: Route.ComponentProps) => {
-  return (
-    <HydrationBoundary state={loaderData.dehydratedUserState}>
-      <UserPage />
-    </HydrationBoundary>
-  )
+export default function UsersRoute() {
+  return <UserPage />
 }
